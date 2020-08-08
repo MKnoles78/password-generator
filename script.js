b8ea48647f6b27951bbdbf59bf590611243720df
Mike Knoles <mknoles78@gmail.com>
2020-08-07 22:44:28 -0400

    tested function for generatePassword to console.log the button was clicked

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numeric = ["1234567890"];
 var symbol = ["!@#$%^&*()"];
 
+// the code provided provides a event listener for for generate button, which triggers the writePassword function.
+// within the writePassword function, var password triggers the generatePassword function, and the va passwordText, writes the information to the id #password found on the index.html
+// a function for generatePassword needs to be created so that it can be called within the writePassword function
+
+function generatePassword () {
+  console.log ("you clicked me")
+  // added a console log to confirm the button was being clicked 
+  // within the generate password function there needs to be a function that combines all the criteria for the password 
+
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -14,8 +25,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  console.log ("you clicked me")
-
+  
 }
   
 
