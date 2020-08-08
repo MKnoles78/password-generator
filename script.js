beef418f72bd47b6e765da7f95c9b53d0e9522ce
Mike Knoles <mknoles78@gmail.com>
2020-08-07 22:30:49 -0400

    updated js file to remove back to the basics

--- script.js
+++ script.js
@@ -1,80 +1,12 @@
 // Assignment Code
-//possilbe variables for password selection
-var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "1234567890";
-var symbol = "!@#$%^&*()";
-var confirmLength;
-var userPassword = [randomLower(confirmLength) + randomUpper(confirmLength) + randomNumber(confirmLength) + randomSymbol(confirmLength)]
+//establish the variables in an array to be used in the password
+var alphaLower = ["abcdefghijklmnopqrstuvwxyz"];
+var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numeric = ["1234567890"];
+var symbol = ["!@#$%^&*()"];
 
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var confirmLength = prompt(
-    "Choose a password length between 8 and 128 characters?"
-  );
-  if (confirmLength >= 8) {
-  } else {
-    alert("Please refresh and reinput between 8 and 128");
-  }
-  
-  var userLower = confirm("Would you like lowercase letters?");
-  
-  function randomLower(l) {
-    var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-    var text = "";
-    for (var i = 0; i < l; i++) {
-      text += alphaLower.charAt(Math.floor(Math.random() * alphaLower.length));
-    }
-    return text;
-  }
-  
-  console.log(randomLower(confirmLength));
-  
-  
-  var userUpper = confirm("Would you like uppercase letters?");
-  
-  function randomUpper(l) {
-    var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var text = "";
-    for (var i = 0; i < l; i++) {
-      text += alphaUpper.charAt(Math.floor(Math.random() * alphaUpper.length));
-    }
-    return text;
-  }
-  console.log(randomUpper(confirmLength));
-  console.log(userUpper)
-  
-  var userNumber = confirm("Would you like numbers?");
-  
-  function randomNumber(l) {
-    var numeric = "1234567890";
-    var text = "";
-    for (var i = 0; i < l; i++) {
-      text += numeric.charAt(Math.floor(Math.random() * numeric.length));
-    }
-    return text;
-  }
-  console.log(randomNumber(confirmLength));
-  
-  
-  var userSymbol = confirm("Would you like spedial characters? (ex. !@#$)");
-  
-  function randomSymbol(l) {
-    var symbol = "!@#$%^&*()";
-    var text = "";
-    for (var i = 0; i < l; i++) {
-      text += symbol.charAt(Math.floor(Math.random() * symbol.length));
-    }
-    return text;
-  }
-  console.log(randomSymbol(confirmLength));
-
-
-// Write password to the #password input
-function generatePassword() {
-  return [randomLower(confirmLength) + randomUpper(confirmLength) + randomNumber(confirmLength) + randomSymbol(confirmLength)]
-
-}
 
 
 function writePassword() {
@@ -82,6 +14,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  console.log ("you clicked me")
 
 }
   
