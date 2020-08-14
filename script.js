733282984eac89f1bbf15c2bfd37cd7761ea49ae
Mike Knoles <mknoles78@gmail.com>
2020-08-13 21:26:36 -0400

    updated special, numeric, alphas characters

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Assignment Code
 //establish the variables in an array to be used in the password
-var alphaLower = ["abcdefghijklmnopqrstuvwxyz"];
-var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numeric = ["1234567890"];
-var symbol = ["!@#$%^&*()"];
+var specialCharacters = ['!','@','#','$','^','&','*']
+var numericCharacters = ['1','2','3','4','5','6','7','8','9','0']
+var alphaLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','s','t','u','v','w','x','y','z'];
+var alphaUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','S','T','U','V','W','X','Y','Z'];
+
 
 // the code provided provides a event listener for for generate button, which triggers the writePassword function.
 // within the writePassword function, var password triggers the generatePassword function, and the va passwordText, writes the information to the id #password found on the index.html
@@ -16,7 +17,8 @@ function generatePassword () {
   console.log ("you clicked me")
   // added a console log to confirm the button was being clicked 
   // within the generate password function there needs to be a function that combines all the criteria for the password 
-  prompt("How many characters would you like for your password to contain?")
+  var passwordLength = prompt("How many characters would you like for your password to contain?")
+  console.log (passwordLength)
   confirm("Would you like special characters?")
   confirm("Would you like numeric characters?")
   confirm("Would you like lowercase characters?")
