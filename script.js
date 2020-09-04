61a82489c61df72339af6fa21e5936a6fbfce9fa
Mike Knoles <mknoles78@gmail.com>
2020-09-04 10:06:04 -0400

    added in conditional statements

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var alphaUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P
 // then a confirm window to confirm special characters, numeric characters, lowercase character, and uppercase characters
 
 
-function generatePassword () {
+function getPasswordOptions () {
   console.log ("you clicked me")
   // added a console log to confirm the button was being clicked 
   // within the generate password function there needs to be a function that combines all the criteria for the password 
@@ -22,13 +22,45 @@ function generatePassword () {
     );
   console.log (passwordLength)
 
+
+//Conditional statement to check to see if password length is a number. Prompts end if this evaluates false
+  if (isNaN(length) === true) {
+    alert('Password length must be provided as a number.');
+    return;
+  }
+  
+  //Condtional statement to check if password is at least 8 characters long.  Prompts end if this evaluates false
+  if (length > 8) {
+    alert('Password length must be at least 8 characters.');
+    return;
+  }
+
+  //Conditional statement to check if password is a less than 128 characters long.  Prompts end if this evaluates false.
+  if (length < 128) {
+    alert('Password lenght must be less than 128 characters.');
+    return;
+  }
+
+
+
   //assign values to var for confirm selections
-  var hasSpecial = confirm("Would you like special characters?");
-  var hasNumeric = confirm("Would you like numeric characters?");
-  var hasLowercase = confirm("Would you like lowercase characters?");
-  var hasUppercase = confirm("Would you like uppercase characters?");
+  var hasSpecial = confirm("Would you like special characters? Click 'OK' to confirm");
+  var hasNumeric = confirm("Would you like numeric characters? Click 'OK' to confirm");
+  var hasLowercase = confirm("Would you like lowercase characters? Click 'OK' to confirm");
+  var hasUppercase = confirm("Would you like uppercase characters? Click 'OK' to confirm");
 
   console.log (hasSpecial, hasNumeric, hasLowercase, hasUppercase)
+
+  //Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false.
+  if (
+    hasSpecial === false &&
+    hasNumeric === false &&
+    hasLowercase === false &&
+    hasUppercase === false 
+  ) {
+    alert('Must select at least one character type.');
+    return;
+  }
 
   // stores values for prompt and confirm selections
   var passwordOptions = {
@@ -46,6 +78,14 @@ function generatePassword () {
 
 //Function for getting a random element from an array
 function getRandom(arr) {
+
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randIndex];
+
+  return randomElement;
+}
+
+function generatePassword() {
   var options = getPasswordOptions();
   var results = [];
 
